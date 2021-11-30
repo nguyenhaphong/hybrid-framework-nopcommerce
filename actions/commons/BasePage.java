96a62676eeae0910cf868cc3ab70290c08c333e3
Nguyen Phong <nguyenhaphong.3108@gmail.com>
2021-11-30 20:34:19 +0700

    BasePage

--- actions/commons/BasePage.java
+++ actions/commons/BasePage.java
@@ -1,5 +1,59 @@
 package commons;
 
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 public class BasePage {
+	public void openUrl (WebDriver driver, String url) {
+		driver.get(url);
+	}
+	
+	public String getPageTitle(WebDriver driver) {
+		return driver.getTitle();
+	}
+	
+	public String getPageUrl(WebDriver driver) {
+		return driver.getCurrentUrl();
+	}
+	
+	public String getPageSourceCode(WebDriver driver) {
+		return driver.getPageSource();
+	}
+	
+	public void backToPage(WebDriver driver) {
+		driver.navigate().back();
+	}
+	
+	public void refreshCurrentPage(WebDriver driver) {
+		driver.navigate().refresh();
+	}
+	
+	public void forwardToPage(WebDriver driver) {
+		driver.navigate().forward();
+	}
+	
+	public Alert waitAlertPresence(WebDriver driver) {
+		WebDriverWait explicitWait = new WebDriverWait(driver, 30);
+		return explicitWait.until(ExpectedConditions.alertIsPresent());
+	}
+	
+	public void acceptAlert (WebDriver driver) {
+		waitAlertPresence(driver).accept();
+	}
+	
+	public void cancelAlert (WebDriver driver) {
+		waitAlertPresence(driver).dismiss();
+	}
+	
+	public String getAlertText (WebDriver driver) {
+		return waitAlertPresence(driver).getText();
+	}
+	
+	public void sendkeyToAlert(WebDriver driver, String value) {
+		waitAlertPresence(driver).sendKeys(value);
+	}
+
 
 }
